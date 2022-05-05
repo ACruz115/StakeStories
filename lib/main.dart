3a40292b46fdea8d36b4c0947352a1bff7bcd179
Andres Cruz <andrescruz201502@gmail.com>
2022-05-05 23:12:55 +0200

    Renamed after our project name

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const StakeStories());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class StakeStories extends StatelessWidget {
+  const StakeStories({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
